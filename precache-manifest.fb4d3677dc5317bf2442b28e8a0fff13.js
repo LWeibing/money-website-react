2a722fd8a28bee41a88e8a8f1b9000ad6fd39f06
Clown <1104480806@qq.com>
2020-08-23 23:06:25 +0800

    update

---
+++ precache-manifest.fb4d3677dc5317bf2442b28e8a0fff13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0508a71850916cac9320378cc735dee2",
+    "url": "/money-website-react/index.html"
+  },
+  {
+    "revision": "978bfd630b21a4fe3dfe",
+    "url": "/money-website-react/static/css/main.34a21201.chunk.css"
+  },
+  {
+    "revision": "db9d594dea144a8d7c38",
+    "url": "/money-website-react/static/js/2.9dfb9f99.chunk.js"
+  },
+  {
+    "revision": "9cade88c6449a828092f3a5a90795c16",
+    "url": "/money-website-react/static/js/2.9dfb9f99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978bfd630b21a4fe3dfe",
+    "url": "/money-website-react/static/js/main.1a7f9619.chunk.js"
+  },
+  {
+    "revision": "ca5e57913c9013ecbcf7",
+    "url": "/money-website-react/static/js/runtime-main.e2be7b17.js"
+  }
+]);
